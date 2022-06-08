0d93a37385e52dca509f3499267e0f1d4308adfd
Victor <victor.adeyemo@gmail.com>
2022-06-07 18:43:00 -0700

    Add two more files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,27 @@
 #include "main.h"
-
 /**
- * main - Entry point
  *
- * Return: Always 0 (Success)
- */
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 int main(void)
+
 {
-char c[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
-int n = 0;
-while (n <= 7)
-{
-putchar(c[n]);
-n++;
-}
+
+ _putchar('_');
+ _putchar('p');
+ _putchar('u');
+ _putchar('t');
+ _putchar('c');
+ _putchar('h');
+ _putchar('a');
+ _putchar('r');
+ _putchar('\n');
+		         
 return (0);
+
 }
