0c960e7e4719150316679083f81b88e0f52e6481
Victor <victor.adeyemo@gmail.com>
2022-06-08 08:04:44 -0700

    Add the program is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
-int _islower(int c)
 /**
- * islowe - Check if a number is greater than 0
- * @c: The parameter sent
- * @a: THis variable
- * @z: an operand
-
+ * _islower - A function that checks for c lowercase
+ * @c: contains value to be compared
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
 if (c > 'a' && c < 'z')
 {
 return (1);
 }
+
 else
 {
 return (0);
