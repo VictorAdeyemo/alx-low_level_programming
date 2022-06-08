f75b5f60f6e9bb658c3e5d00ae5f2259ef802dc6
Victor <victor.adeyemo@gmail.com>
2022-06-08 12:44:17 -0700

    Function 6-abs added

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main"
+
+/**
+ *  print_last_digit - Function prints the last digit of a number
+ *
+ *  @n: The number to be evaluated
+ *
+ *  Return: Return the last digit of the number
+ *
+ */
+
+int print_last_digit(int n)
+{
+return (n%10);
+}
