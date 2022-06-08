3718121ba3671d14a186a47f2b2f8b6e6ad44f89
Victor <victor.adeyemo@gmail.com>
2022-06-08 12:25:39 -0700

    Uploaded Abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _abs - a function that prints the sign of a number
+ *
+ * @n: The number to be evaluated
+ *
+ * Description: a function that prints the sign of a number
+ *
+ *
+ * Return: n .
+ */
+
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+
+else
+{
+return (n * (-1));
+}
+}
