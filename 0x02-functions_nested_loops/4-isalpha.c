52fd5a867c4f3aeb5d2b3686b1ced3b853af4217
Victor <victor.adeyemo@gmail.com>
2022-06-08 08:13:34 -0700

    Add the isalpha file

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,29 +1,26 @@
 #include "main.h"
 
 /**
+ * _isalpha - Check for alphabetic character
  *
- * main - check the code
+ * @c: An input character to be checked
  *
- * Return: Always 0.
+ * Description: a function that checks for alphabetic character.
+ *
+ *
+ * Return: 1 or 0 .
  */
 
 int _isalpha(int c)
-
 {
 
-if ((c >'a' && c < 'z') || (c > 'A' && c < 'z'))
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+	{
+		return (1);
+	}
 
-{
-
-return (1);
-
+	else
+	{
+		return (0);
+	}
 }
-
-else
-
-{
-
-return (0);
-
-}
-
