3a015b174fdded4063dbd82271623a00da735c07
Victor <victor.adeyemo@gmail.com>
2022-06-10 14:58:38 -0700

    Fix the bug

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+/**
+ * _isdigit -  a function that checks for a digit.
+ *
+ * @c: contains value to be checked
+ *
+ * Description: checks for a digit using the ASCII value.
+ *
+ * Return: 1 if is digit otherwise return 0
+ */
+
+int _isdigit(int c)
+{
+
+	if (c >= 48 && c <= 57)
+
+                return (1);
+
+	else
+		return (0);
+}
