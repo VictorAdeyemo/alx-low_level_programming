e7591be9934774689974d2acee0046e3be04e2d8
Victor <victor.adeyemo@gmail.com>
2022-06-10 16:12:59 -0700

    printnumbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+/**
+ * print_numbers -  a function that print 0 to 9.
+ *
+ * @n: counter starting point
+ *
+ * Description: Prints numbers 0 - 9.
+ *
+ * Return: Always 0 (Success).
+ */
+
+void print_numbers(void)
+{
+int n = 0;
+
+	do {
+		printf("%i", n);
+		n = n + 1;
+}
+	while (n < 10);
+
+	return (0);
+}
