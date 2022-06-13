0b84036222ce3d852a1ab237cb6e1f72a6e16fe1
Victor <victor.adeyemo@gmail.com>
2022-06-13 13:32:48 -0700

    Resolved

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,14 +4,12 @@
 *swap_int - Swap the values of two integers
 *@a: First parameter
 *@b: Second parameter
-*@m: Temporary pointer to hold the value of *a
 *
 *Return : nothing
 */
 void swap_int(int *a, int *b)
 {
-	int m = *a;
 	*a = *b;
-	*b = m;
+	*b = *a;
 
 }
