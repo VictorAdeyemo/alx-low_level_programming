61b00390e065a43a935a761b8620d516e2897b2e
Victor <victor.adeyemo@gmail.com>
2022-06-13 12:14:44 -0700

    Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - This function reset the value pointer n points to
+ *
+ * @n: The pointer variable to be manipulated
+ *
+ * Return: Return the updated value of n
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	&n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.d
@@ -0,0 +1,1 @@
+In this directory, I have the tasks for project 0x05.c - Pointers,arrays and strings.
