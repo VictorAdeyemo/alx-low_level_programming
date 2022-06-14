ea323126edf44a2627e83ea8eb69a6afd52eec2b
Victor <victor.adeyemo@gmail.com>
2022-06-13 18:40:46 -0700

    Reverse order

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* print_rev - prints a given string in reverse order
+*
+* @s: The given string
+*
+*/
+
+
+
+void print_rev(char *s)
+
+{
+
+	int i = 0;
+
+	int j;
+
+
+
+	while (s[i] != '\0')
+
+	{
+
+		i++;
+
+	}
+
+	for (j = i - 1; j >= 0; j--)
+
+	{
+
+		_putchar(s[j]);
+
+	}
+
+	_putchar('\n');
+
+}
