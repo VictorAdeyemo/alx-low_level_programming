aa25453bf40bd3bfa03806d0c89599956a873157
Victor <victor.adeyemo@gmail.com>
2022-06-21 06:08:45 -0700

    Memory copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - Memory copy
+ * @dest: Memory destination
+ * @src: Memory source
+ * @n: number of bytes to be copied
+ *
+ * Return: Return
+ *
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	usigned int a;
+
+	for (a = 0 ; a < n ; a++)
+		dest[a] = src[a];
+	return (dest);
+}
