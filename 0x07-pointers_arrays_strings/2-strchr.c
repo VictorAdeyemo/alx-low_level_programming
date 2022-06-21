00dffadd266429bad1670f1b50ee1da0042a03e0
Victor <victor.adeyemo@gmail.com>
2022-06-21 10:40:19 -0700

    Done it

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,22 +5,26 @@
  * @s: The string given
  * @c: The character to be located
  *
- * Return: a pointer to the first occurence of character c otherwise return NUL.
+ * Return: a pointer to the first occurence of c otherwise return NULL.
  *
  */
 
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 		{
 			return (s + i);
 		}
+		else
+		{
+			return (NULL);
+		}
+	i++;
+
 	}
-	if (s[i] == c)
-		return (s + i);
-	return (0);
+}
 }
