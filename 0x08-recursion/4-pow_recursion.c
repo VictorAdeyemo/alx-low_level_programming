4df8def32cd947465ce6698221e06469e2bbf28d
Victor <victor.adeyemo@gmail.com>
2022-06-23 14:35:47 -0700

    Square

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - this function returns the square of a number
+ * @x: The number to be squared
+ * @y: The number of square
+ *
+ * Return: Returns the sqaure of x to power y.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (n ^ y);
+}
