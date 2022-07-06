900f28cc0cb99298884d317435594b25f3f47121
Victor <victor.adeyemo@gmail.com>
2022-07-06 09:59:30 -0700

    Done

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: pointer to function that returns void
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	unsigned int i = 0;
+
+	if (f == NULL)
+	{
+		while (name[i])
+		{
+			_putchar(name[i]);
+			i++;
+		}
+	}
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+Tasks for project: 0x0F. C - Function pointers
